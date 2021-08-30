389b53b9f52b0eba0823bf866049d909fd722e61
Tales Valente <talesvalente@gmail.com>
2021-08-30 10:26:42 -0300

    *Update SRC +Merge content of offline src

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,40 +4,91 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace AuvoTech.Controllers
 {
     public class HomeController : Controller
     {
+        //Define uma instância de IHostingEnvironment
+        IHostingEnvironment _appEnvironment;
+        //Injeta a instância no construtor para poder usar os recursos
+        public HomeController(IHostingEnvironment env)
+        {
+            _appEnvironment = env;
+        }
+        //Retorna a View Index.cshtml que será o formulário para
+        //selecionar os arquivos a serem enviados 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        //método para enviar os arquivos usando a interface IFormFile
+        public async Task<IActionResult> EnviarArquivo(List<IFormFile> arquivos)
+        {
+            long tamanhoArquivos = arquivos.Sum(f => f.Length);
+            // caminho completo do arquivo na localização temporária
+            var caminhoArquivo = Path.GetTempFileName();
+
+            // processa os arquivo enviados
+            //percorre a lista de arquivos selecionados
+            foreach (var arquivo in arquivos)
+            {
+                //verifica se existem arquivos 
+                if (arquivo == null || arquivo.Length == 0)
+                {
+                    //retorna a viewdata com erro
+                    ViewData["Erro"] = "Error: Arquivo(s) não selecionado(s)";
+                    return View(ViewData);
+                }
+
+                // < define a pasta onde vamos salvar os arquivos >
+                string pasta = "Arquivos_Usuario";
+                // Define um nome para o arquivo enviado incluindo o sufixo obtido de milesegundos
+                string nomeArquivo = "File_" + DateTime.Now.Millisecond.ToString();
+
+                //verifica qual o tipo de arquivo enviado
+                if (arquivo.FileName.Contains(".ofx"))
+                {
+                    nomeArquivo += ".ofx";
+                }
+                else
+                {
+                    ViewData["Error"] = "Aviso: Arquivo Não Suportado. Somente arquivos do tipo '.ofx' são aceitos na aplicação. Tente novamente.";
+                    return View(ViewData);
+                }
+
+                //< obtém o caminho físico da pasta wwwroot >
+                string caminho_WebRoot = _appEnvironment.WebRootPath;
+                // monta o caminho onde vamos salvar o arquivo : 
+                // ~\wwwroot\Arquivos\Arquivos_Usuario\Recebidos
+                string caminhoDestinoArquivo = caminho_WebRoot + "\\Arquivos\\" + pasta + "\\";
+                // incluir a pasta Recebidos e o nome do arquivo enviado : 
+                // ~\wwwroot\Arquivos\Arquivos_Usuario\Recebidos\
+                string caminhoDestinoArquivoOriginal = caminhoDestinoArquivo + "\\Recebidos\\" + nomeArquivo;
+                //copia o arquivo para o local de destino original
+                using (var stream = new FileStream(caminhoDestinoArquivoOriginal, FileMode.Create))
+                {
+                    await arquivo.CopyToAsync(stream);
+                }
+            }
+
+            //monta a ViewData que será exibida na view como resultado do envio 
+            ViewData["Resultado"] = $"{arquivos.Count} arquivos foram enviados ao servidor, " +
+             $"com tamanho total de : {tamanhoArquivos} bytes";
+
+            //retorna a viewdata
+            return View(ViewData);
+        }
+
     }
 }
